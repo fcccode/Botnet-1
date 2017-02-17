e429478dd2f3cc310ab361a5bb4e1b635c395f7f
MegaXLR <admin@megaxlr.net>
2017-02-17 10:10:29 +0100

    Initial Commit
    Use ./botnet to print
    
    use ./botnet --Super to print a SUPER BOTNET
    
    Signed-off-by: MegaXLR <admin@megaxlr.net>

---
+++ botnet.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char const *argv[]) {
+  if(argc < 2) {
+    printf("\nBOTNET\nO\nT\nN\nE\nT\n\n");
+  } else if (strcmp(argv[1], "--Super") == 0) {
+    printf("\n ____   ____ _______ _   _ ______ _______\n");
+    printf("|  _ \\ / __ \\__   __| \\ | |  ____|__   __|\n");
+    printf("| |_) | |  | | | |  |  \\| | |__     | |\n");
+    printf("|  _ <| |  | | | |  | . ` |  __|    | |\n");
+    printf("| |_) | |__| | | |  | |\\  | |____   | |\n");
+    printf("|____/ \\____/  |_|  |_| \\_|______|  |_|\n");
+    printf(" / __ \\\n");
+    printf("| |  | |\n");
+    printf("| |  | |\n");
+    printf("| |__| |\n");
+    printf(" \\____/_\n");
+    printf("|__   __|\n");
+    printf("   | |\n");
+    printf("   | |\n");
+    printf("   | |\n");
+    printf(" _ |_|\n");
+    printf("| \\ | |\n");
+    printf("|  \\| |\n");
+    printf("| . ` |\n");
+    printf("| |\\  |\n");
+    printf("|_|_\\_|\n");
+    printf("|  ____|\n");
+    printf("| |__\n");
+    printf("|  __|\n");
+    printf("| |____\n");
+    printf("|______|\n");
+    printf("|__   __|\n");
+    printf("   | |\n");
+    printf("   | |\n");
+    printf("   | |\n");
+    printf("   |_|\n\n");
+  }
+
+  return 0;
+}
